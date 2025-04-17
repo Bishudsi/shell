999c1fc84de870881fe44d7e8c7d3bd263e9708e
Bishudsi <bmana@gmail.com>
2025-04-17 17:48:29 -0400

    Updated as a part of Assignment 16Apr2025

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -21,21 +21,31 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
+     mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
+     mv rawdata data/raw
 
 # 3. List the contents of the ./data/raw directory
+     ls data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+     mkdir processed
+     mkdir processed/server_logs
+     mkdir processed/user_logs  
+     mkdir processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
+     
+     cp /Users/bmanna/Desktop/diswrk1/shell/02_activities/assignments/data/raw/server_log* /Users/bmanna/Desktop/diswrk1/shell/02_activities/assignments/data/processed/server_logs 
 # 6. Repeat the above step for user logs and event logs
+     cp /Users/bmanna/Desktop/diswrk1/shell/02_activities/assignments/data/raw/event_log* /Users/bmanna/Desktop/diswrk1/shell/02_activities/assignments/data/processed/event_logs 
+     cp /Users/bmanna/Desktop/diswrk1/shell/02_activities/assignments/data/raw/user_log* /Users/bmanna/Desktop/diswrk1/shell/02_activities/assignments/data/processed/user_logs 
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-
+     rm -rf data/raw/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+     ls -lR data/processed > data/inventory.txt
 
 ###########################################
 
